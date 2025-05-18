16739e4f64970116ec977b53442e68ebd18fc36e
Shiva Adhikari <s2whl6xh@duck.com>
2025-05-18 23:10:18 +0545

    refactor: replace old sync logic with real-time diff-match-patch implementation in js

--- src/main.js
+++ src/main.js
@@ -1,36 +1,65 @@
 window.addEventListener("DOMContentLoaded", () => {
     const websocket = new WebSocket("ws://localhost:6789/");
-    const textarea = document.getElementById("textarea_id");
+    const textArea = document.getElementById("textarea_id");
+
+    const dmp = new diff_match_patch();
 
     // Store last sent text to avoid duplicate sends
-    let send_text = "";
+    let prevText = "";
     let debounceTimer;
     const DEBOUNCE_DELAY = 500; // 500ms delay
 
     // Add event listener for when WebSocket connection is open
     websocket.addEventListener("open", () => {
       // Add event listener for textarea input changes
-      textarea.addEventListener("input", () => {
-        const text = textarea.value;
+      textArea.addEventListener("input", () => {
 
         // Clear any pending timeout
         clearTimeout(debounceTimer);
 
         // Set a new timeout
         debounceTimer = setTimeout(() => {
-          // Only send if text has changed from last sent text
-          if (text !== send_text) {
-            websocket.send(text);
-            send_text = text; // Update the last sent text
-          }
+            const currentText = textArea.value;
+
+            if (currentText != prevText) {
+                // generate patch from prevText to currentText
+                const patches = dmp.patch_make(prevText, currentText);
+                const patchText = dmp.patch_toText(patches)
+                // window.alert("client le server lai send garxa: " +patchText);
+
+                // send patch
+                websocket.send(patchText);
+            }
         }, DEBOUNCE_DELAY);
       });
     });
 
+
     // if server sends text, update text to textarea
     websocket.addEventListener("message", (event) => {
-        textarea.value = event.data;
+        const patchText = event.data;
+        // window.alert("server bata aako: " +patchText);
+
+        try {
+            const patches = dmp.patch_fromText(patchText);
+            const [newText, results] = dmp.patch_apply(patches, prevText);
+
+            // only update if all patches applied successfully
+            if (results.every(r => r === true)) {
+                if (prevText.trim() !== newText.trim()) {
+                    prevText = newText;
+                    textArea.value = prevText;
+                } else {
+                    window.alert("ALREADY UPTODATE")
+                }
+            } else {
+                console.error("Some patches failed to apply:", results);
+            }
+        } catch (e) {
+            console.error('Failed to apply patch', e);
+        }    
     });
+
 
     // check error
     websocket.addEventListener("error", () => {
