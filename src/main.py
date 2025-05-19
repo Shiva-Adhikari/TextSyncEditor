489b15d10bc43f2bb024568bd0511b9eda0baec1
Shiva Adhikari <s2whl6xh@duck.com>
2025-05-20 00:46:48 +0545

    feat: implemented fastapi

--- src/main.py
+++ src/main.py
@@ -4,31 +4,46 @@ import asyncio
 import logging
 
 # Third party Module
-import diff_match_patch
-from websockets.asyncio.server import serve
+from diff_match_patch import diff_match_patch
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 
 
-logging.basicConfig(
-    level=logging.INFO,
-    format='%(levelname)s %(message)s'
-)
+app = FastAPI()
 
-root_path = os.path.abspath('data')
-os.makedirs(root_path, exist_ok=True)
-file_path = os.path.join(root_path, 'text.txt')
-dmp = diff_match_patch.diff_match_patch()
+dmp = diff_match_patch()
+logging.basicConfig(level=logging.INFO, format='%(levelname)s %(message)s')
+
+app.mount('/static', StaticFiles(directory='src'), name='static')
+
+data_path = os.path.abspath('data')
+os.makedirs(data_path, exist_ok=True)
+file_path = os.path.join(data_path, 'text.txt')
+src_path = os.path.abspath('src')
+html_path = os.path.join(src_path, 'main.html')
 
 
-async def main_py(websocket):
+@app.get('/')
+async def get():
+    with open(html_path, 'r', encoding='utf-8') as f:
+        return HTMLResponse(content=f.read())
+
+
+@app.websocket('/ws')
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
     current_text = ''
+
     if os.path.exists(file_path):
         with open(file_path, 'r', encoding='utf-8') as f:
             current_text = f.read()
 
     async def server_receive():
         nonlocal current_text
-        async for message in websocket:
+        while True:
             try:
+                message = await websocket.receive_text()
                 logging.info(f'\nmessage: {message}\n\n')
                 patches = dmp.patch_fromText(message)
                 # creating an updated version. by changes of current and get updated
@@ -42,39 +57,31 @@ async def main_py(websocket):
                         current_text = updated_text
                 else:
                     logging.warning('Some patch failed to apply')
+            except WebSocketDisconnect:
+                logging.info('Client disconnected')
             except Exception as e:
                 logging.error(f'Failed to apply patch {e}')
 
     async def server_send():
         nonlocal current_text
         prev_text = ''
-        while True:
-            if os.path.exists(file_path):
-                with open(file_path, 'r', encoding='utf-8') as f:
-                    current_text = f.read()
-            await asyncio.sleep(.5)
-            if current_text != prev_text:
-                # is used to compare both, which is added, removed or unchanged
-                patches = dmp.patch_make(prev_text, current_text)
-                patch_text = dmp.patch_toText(patches)
-                logging.info(f'yo chai server bata client ma send gareko: {patch_text}')
-                await websocket.send(patch_text)
-                prev_text = current_text
+        try:
+            while True:
+                if os.path.exists(file_path):
+                    with open(file_path, 'r', encoding='utf-8') as f:
+                        current_text = f.read()
+                await asyncio.sleep(.5)
+                if current_text != prev_text:
+                    # is used to compare both, which is added, removed or unchanged
+                    patches = dmp.patch_make(prev_text, current_text)
+                    patch_text = dmp.patch_toText(patches)
+                    logging.info(f'yo chai server bata client ma send gareko: {patch_text}')
+                    await websocket.send_text(patch_text)
+                    prev_text = current_text
+        except WebSocketDisconnect:
+            logging.info('Client disconnected')
+        except Exception as e:
+            logging.error(f'Error while sending patch {e}')
 
     # gather is used to run function in parallel
-    try:
-        await asyncio.gather(server_send(), server_receive())
-    except Exception as e:
-        logging.error(f'Connection closed: {e}')
-    finally:
-        await websocket.close()
-
-
-async def main():
-    async with serve(main_py, 'localhost', 6789) as server:
-        logging.info('Starting...')
-        await server.serve_forever()
-
-
-if __name__ == '__main__':
-    asyncio.run(main())
+    await asyncio.gather(server_send(), server_receive())
